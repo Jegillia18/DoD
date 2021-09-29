e38c5d9b643921ac6dddfb8d5da6d8f520017a68
Jessica Gilliam <82763596+Jegillia18@users.noreply.github.com>
2021-09-29 15:12:46 -0400

    Data for Method 2

--- Evan/MEP_Value_Pull.m
+++ Evan/MEP_Value_Pull.m
@@ -1,9 +1,24 @@
 %% Import data from spreadsheet
 % Script for importing data from the following spreadsheet:
 %
-%    Workbook: C:\Users\cazmo\Desktop\DoD Muscle Extraction\MCD_pre\691_P10_pre_corticalexcitability-MCD data.xlsx
-%    Worksheet: Averages
-%
+%    Method 1:
+% Onset1: 3 consecutive points above MCD
+% Offset1: 3 consecutive points below MCD
+% Onset2: 3 consecutive points above MCD
+% Offset2: 3 consecutive points below MCD
+% 
+%    Method 2:
+% Onset1: 5 consecutive points above MCD
+% Offset1: 5 consecutive points below MCD
+% Onset2: 5 consecutive points above MCD
+% Offset2: 5 consecutive points below MCD
+
+
+% Updates need to be made to MEP area under the curve,Duration,add excel
+% output file automation
+
+% ANY CHANGES: need to be copied over to method 3/4
+
 clear
 clc
 
@@ -96,59 +111,131 @@ MCD = temp_tv(2,1);
 SD = temp_tv(2,2);
 
 
-%MCD
+%MCD : Onset 1 
 for i = start:(length(waveform)-5)
     %Below written like: wave(1) > MCD value ----- wave(2)> MCD value
- if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD% && waveform(i+3) > MCD && waveform(i+4) > MCD
-     MCD_onset_index = i;
-     MCD_onset_time = time(i);
+ if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD && waveform(i+3) > MCD && waveform(i+4) > MCD
+     MCD_onset1_index = i;
+     MCD_onset1_time = time(i);
      break
      else
-        MCD_onset_index = NaN;
-        MCD_onset_time = NaN;
+        MCD_onset1_index = NaN;
+        MCD_onset1_time = NaN;
  end
 end
 
-if isnan(MCD_onset_index)
-     MCD_offset_index = NaN;
-     MCD_offset_time = NaN;
+%MCD : Offset 1 
+if isnan(MCD_onset1_index)
+     MCD_offset1_index = NaN;
+     MCD_offset1_time = NaN;
 else
-    for i = MCD_onset_index:(length(waveform)-5) 
-        if waveform(i) < MCD && waveform(i+1) < MCD && waveform(i+2) < MCD% && waveform(i+3) < MCD && waveform(i+4) < MCD
-            MCD_offset_index = i;
-            MCD_offset_time = time(i);
+    for i = MCD_onset1_index:(length(waveform)-5) 
+        if waveform(i) < MCD && waveform(i+1) < MCD && waveform(i+2) < MCD && waveform(i+3) < MCD && waveform(i+4) < MCD
+            MCD_offset1_index = i;
+            MCD_offset1_time = time(i);
         break
         else
-            MCD_offset_index = NaN;
-            MCD_offset_time = NaN; 
+            MCD_offset1_index = NaN;
+            MCD_offset1_time = NaN; 
         end
     end
 end
 
-%2SD
+%MCD : Onset 2
+if isnan(MCD_offset1_index)
+    MCD_onset2_index = NaN;
+    MCD_onset2_time = NaN;
+    
+else
+    for i = MCD_offset1_index: (length(waveform)-5)
+        if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD && waveform(i+3) < MCD && waveform(i+4) < MCD
+            MCD_onset2_index = i;
+            MCD_onset2_time = time(i);
+        break
+        else
+            MCD_onset2_index = NaN;
+            MCD_onset2_time = NaN;
+        end
+    end
+end
+
+%MCD : Offset 2
+if isnan(MCD_onset2_index)
+    MCD_offset2_index = NaN;
+    MCD_offset2_time = NaN;
+else
+    for i = MCD_onset2_index: (length(waveform)-5)
+        if waveform(i) < MCD && waveform(i+1) < MCD && waveform(i+2) < MCD && waveform(i+3) < MCD && waveform(i+4) < MCD
+            MCD_offset2_index = i;
+            MCD_offset2_time = time(i);
+        break
+        else
+            MCD_offset2_index = NaN;
+            MCD_offset2_time = NaN;
+        end
+    end
+end
+            
+
+%SD : Onset 1
 for i = start:(length(waveform)-5) 
- if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD% && waveform(i+3) > SD && waveform(i+4) > SD
-     SD_onset_index = i;
-     SD_onset_time = time(i);
+ if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
+     SD_onset1_index = i;
+     SD_onset1_time = time(i);
      break
      else
-        SD_onset_index = NaN;
-        SD_onset_time = NaN;
+        SD_onset1_index = NaN;
+        SD_onset1_time = NaN;
  end
 end
 
-if isnan(SD_onset_index)
-     SD_offset_index = NaN;
-     SD_offset_time = NaN;
+%SD : Offset 1
+if isnan(SD_onset1_index)
+     SD_offset1_index = NaN;
+     SD_offset1_time = NaN;
 else
-    for i = SD_onset_index:(length(waveform)-5) 
-        if waveform(i) < SD && waveform(i+1) < SD && waveform(i+2) < SD% && waveform(i+3) < SD && waveform(i+4) < SD
-            SD_offset_index = i;
-            SD_offset_time = time(i);
+    for i = SD_onset1_index:(length(waveform)-5) 
+        if waveform(i) < SD && waveform(i+1) < SD && waveform(i+2) < SD && waveform(i+3) < SD && waveform(i+4) < SD
+            SD_offset1_index = i;
+            SD_offset1_time = time(i);
         break
         else
-            SD_offset_index = NaN;
-            SD_offset_time = NaN; 
+            SD_offset1_index = NaN;
+            SD_offset1_time = NaN; 
+        end
+    end
+end
+
+%SD : Onset 2
+if isnan(SD_offset1_index)
+     SD_onset2_index = NaN;
+     SD_onset2_time = NaN; 
+else
+    for i = SD_offset1_index: (length(waveform)-5)
+        if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
+         SD_onset2_index = i;
+         SD_onset2_time = time(i);
+     break
+     else
+        SD_onset2_index = NaN;
+        SD_onset2_time = NaN;
+        end
+    end
+end
+
+%SD : Offset 2
+if isnan(SD_onset2_index)
+     SD_offset2_index = NaN;
+     SD_offset2_time = NaN;
+else
+    for i = SD_onset2_index:(length(waveform)-5) 
+        if waveform(i) < SD && waveform(i+1) < SD && waveform(i+2) < SD && waveform(i+3) < SD && waveform(i+4) < SD
+            SD_offset2_index = i;
+            SD_offset2_time = time(i);
+        break
+        else
+            SD_offset2_index = NaN;
+            SD_offset2_time = NaN; 
         end
     end
 end
@@ -162,23 +249,67 @@ hold on
 yline(SD,'g--');
 
 filename = sprintf('P%d_MEP_Plot',p);
-cd Plots_5
+%cd Plots_M1
+cd Plots_M2
 savefig(filename);
 close
-cd 'C:\Users\jg300416\Documents\MATLAB\DoD\Evan'
+%cd 'C:\Users\jg300416\Documents\MATLAB\DoD\Evan'
+cd 'C:\Users\Jessi\Documents\DoD\Evan'
 
-try MCD_AUC = trapz(waveform(MCD_onset_index:MCD_offset_index));
-catch MCD_AUC = NaN;
+% MEP Area Under the Curve (AUC):
+% Area 1: Onset 1 – Offset 1
+try MCD_AUC1 = trapz(waveform(MCD_onset1_index:MCD_offset1_index));
+catch MCD_AUC1 = NaN;
 end
 
-try SD_AUC = trapz(waveform(SD_onset_index:SD_offset_index));
-catch SD_AUC = NaN;
+try SD_AUC1 = trapz(waveform(SD_onset1_index:SD_offset1_index));
+catch SD_AUC1 = NaN;
 end
 
-Parameters(y,:) = [p MCD SD MCD_onset_time MCD_offset_time SD_onset_time SD_offset_time MCD_AUC SD_AUC];
+% Area 2: Onset 2 – Offset 2
+try MCD_AUC2 = trapz(waveform(MCD_onset2_index:MCD_offset2_index));
+catch MCD_AUC2 = NaN;
+end
+
+try SD_AUC2 = trapz(waveform(SD_onset2_index:SD_offset2_index));
+catch SD_AUC2 = NaN;
+end
+
+% Area 3: Onset 1 – Offset 2
+try MCD_AUC3 = trapz(waveform(MCD_onset1_index:MCD_offset2_index));
+catch MCD_AUC3 = NaN;
+end
+
+try SD_AUC3 = trapz(waveform(SD_onset1_index:SD_offset2_index));
+catch SD_AUC3 = NaN;
+end
+
+
+
+Parameters(y,:) = [p MCD SD MCD_onset1_time MCD_offset1_time MCD_onset2_time...
+    MCD_offset2_time SD_onset1_time SD_offset1_time SD_onset2_time SD_offset2_time ...
+    MCD_AUC1 SD_AUC1 MCD_AUC2 SD_AUC2 MCD_AUC3 SD_AUC3];
 
 clearvars -except Parameters y set 
 end
 
 
-
+%filename = 'Data_Method_1.xlsx';
+filename = 'Data_Method_2.xlsx';
+xlswrite(filename,Parameters(:,1),'Sheet1','A2')
+xlswrite(filename,Parameters(:,2),'Sheet1','B2')
+xlswrite(filename,Parameters(:,3),'Sheet1','C2')
+xlswrite(filename,Parameters(:,4),'Sheet1','D2')
+xlswrite(filename,Parameters(:,5),'Sheet1','E2')
+xlswrite(filename,Parameters(:,6),'Sheet1','F2')
+xlswrite(filename,Parameters(:,7),'Sheet1','G2')
+xlswrite(filename,Parameters(:,8),'Sheet1','H2')
+xlswrite(filename,Parameters(:,9),'Sheet1','I2')
+xlswrite(filename,Parameters(:,10),'Sheet1','J2')
+xlswrite(filename,Parameters(:,11),'Sheet1','K2')
+xlswrite(filename,Parameters(:,12),'Sheet1','L2')
+xlswrite(filename,Parameters(:,13),'Sheet1','M2')
+xlswrite(filename,Parameters(:,14),'Sheet1','N2')
+xlswrite(filename,Parameters(:,15),'Sheet1','O2')
+xlswrite(filename,Parameters(:,16),'Sheet1','P2')
+xlswrite(filename,Parameters(:,17),'Sheet1','Q2')
