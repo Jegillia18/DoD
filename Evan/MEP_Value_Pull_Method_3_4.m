f944ab9f522f5d375ee4e38a6b2b2f07b3539eba
Jessica Gilliam <Jessica18gilliam@gmail.com>
2021-10-21 12:53:30 -0400

    Updated version - adjustment to number of peaks located

--- Evan/MEP_Value_Pull_Method_3_4.m
+++ Evan/MEP_Value_Pull_Method_3_4.m
@@ -13,6 +13,7 @@
 % Onset2: 5 consecutive points above MCD
 % Offset2: 3 out of 5 points below MCD
 
+% Total peaks above MCD/SD it will look for is 5, on/off 
 
 clear
 clc
@@ -229,6 +230,204 @@ else
 end
 
 
+%MCD : Onset 3
+if isnan(MCD_offset2_index)
+    MCD_onset3_index = NaN;
+    MCD_onset3_time = NaN;
+    
+else
+    for i = MCD_offset2_index:(length(waveform)-5)
+        if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD && waveform(i+3) > MCD && waveform(i+4) > MCD
+            MCD_onset3_index = i;
+            MCD_onset3_time = time(i);
+        break
+        else
+            MCD_onset3_index = NaN;
+            MCD_onset3_time = NaN;
+        end
+    end
+end
+
+%MCD : Offset 3 for 3/5  
+if isnan(MCD_onset3_index)
+     MCD_offset3_index = NaN;
+     MCD_offset3_time = NaN;
+else
+    B = 0;
+    for i = MCD_onset3_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < MCD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                if c1 == 1 % cycling through to find the first value 
+                    MCD_offset3_index = c(g,1);
+                    MCD_offset3_time = time(c(g,1));
+                    break 
+                else
+                    MCD_offset3_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    MCD_offset3_time = NaN;
+                end
+            end
+         else
+                MCD_offset3_index = NaN; %establishes as a NaN variable 
+                MCD_offset3_time = NaN;
+                       
+         end
+end
+
+
+%MCD : Onset 4
+if isnan(MCD_offset3_index)
+    MCD_onset4_index = NaN;
+    MCD_onset4_time = NaN;
+    
+else
+    for i = MCD_offset3_index:(length(waveform)-5)
+        if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD && waveform(i+3) > MCD && waveform(i+4) > MCD
+            MCD_onset4_index = i;
+            MCD_onset4_time = time(i);
+        break
+        else
+            MCD_onset4_index = NaN;
+            MCD_onset4_time = NaN;
+        end
+    end
+end
+
+%MCD : Offset 4 for 3/5  
+if isnan(MCD_onset4_index)
+     MCD_offset4_index = NaN;
+     MCD_offset4_time = NaN;
+else
+    B = 0;
+    for i = MCD_onset4_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < MCD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                if c1 == 1 % cycling through to find the first value 
+                    MCD_offset4_index = c(g,1);
+                    MCD_offset4_time = time(c(g,1));
+                    break 
+                else
+                    MCD_offset4_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    MCD_offset4_time = NaN;
+                end
+            end
+         else
+                MCD_offset4_index = NaN; %establishes as a NaN variable 
+                MCD_offset4_time = NaN;
+                       
+         end
+end
+
+%MCD : Onset 5
+if isnan(MCD_offset4_index)
+    MCD_onset5_index = NaN;
+    MCD_onset5_time = NaN;
+    
+else
+    for i = MCD_offset4_index:(length(waveform)-5)
+        if waveform(i) > MCD && waveform(i+1) > MCD && waveform(i+2) > MCD && waveform(i+3) > MCD && waveform(i+4) > MCD
+            MCD_onset5_index = i;
+            MCD_onset5_time = time(i);
+        break
+        else
+            MCD_onset5_index = NaN;
+            MCD_onset5_time = NaN;
+        end
+    end
+end
+
+%MCD : Offset 5 for 3/5  
+if isnan(MCD_onset5_index)
+     MCD_offset5_index = NaN;
+     MCD_offset5_time = NaN;
+else
+    B = 0;
+    for i = MCD_onset5_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < MCD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                if c1 == 1 % cycling through to find the first value 
+                    MCD_offset5_index = c(g,1);
+                    MCD_offset5_time = time(c(g,1));
+                    break 
+                else
+                    MCD_offset5_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    MCD_offset5_time = NaN;
+                end
+            end
+         else
+                MCD_offset5_index = NaN; %establishes as a NaN variable 
+                MCD_offset5_time = NaN;
+                       
+         end
+end
+
+
+
 %SD : Onset 1
 for i = start:(length(waveform)-5) 
  if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
@@ -349,6 +548,218 @@ else
          end
 end
 
+%SD : Onset 3
+if isnan(SD_offset2_index)
+     SD_onset3_index = NaN;
+     SD_onset3_time = NaN; 
+else
+    for i = SD_offset2_index: (length(waveform)-5)
+        if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
+         SD_onset3_index = i;
+         SD_onset3_time = time(i);
+     break
+     else
+        SD_onset3_index = NaN;
+        SD_onset3_time = NaN;
+        end
+    end
+end
+
+%SD : Offset 3
+if isnan(SD_onset3_index)
+     SD_offset3_index = NaN;
+     SD_offset3_time = NaN;
+else
+    B = 0;
+    for i = SD_onset3_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < SD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                if c1 == 1 % cycling through to find the first value 
+                    SD_offset3_index = c(g,1);
+                    SD_offset3_time = time(c(g,1));
+                    break
+                else
+                    SD_offset3_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    SD_offset3_time = NaN;
+                end
+            end
+         else
+                SD_offset3_index = NaN; %establishes as a NaN variable 
+                SD_offset3_time = NaN;
+                       
+         end
+end
+
+%SD : Onset 4
+if isnan(SD_offset3_index)
+     SD_onset4_index = NaN;
+     SD_onset4_time = NaN; 
+else
+    for i = SD_offset3_index: (length(waveform)-5)
+        if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
+         SD_onset4_index = i;
+         SD_onset4_time = time(i);
+     break
+     else
+        SD_onset4_index = NaN;
+        SD_onset4_time = NaN;
+        end
+    end
+end
+
+%SD : Offset 4
+if isnan(SD_onset4_index)
+     SD_offset4_index = NaN;
+     SD_offset4_time = NaN;
+else
+    B = 0;
+    for i = SD_onset4_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < SD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                try
+                if c1 == 1 % cycling through to find the first value 
+                    SD_offset4_index = c(g,1);
+                    SD_offset4_time = time(c(g,1));
+                    break
+                else
+                    SD_offset4_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    SD_offset4_time = NaN;
+                end
+                catch
+                    SD_offset4_index = NaN; %establishes as a NaN variable 
+                    SD_offset4_time = NaN;
+                end
+            end
+         else
+                SD_offset4_index = NaN; %establishes as a NaN variable 
+                SD_offset4_time = NaN;
+                       
+         end
+end
+
+%SD : Onset 5
+
+if isnan(SD_offset4_index)
+     SD_onset5_index = NaN;
+     SD_onset5_time = NaN; 
+
+else
+    
+    for i = SD_offset4_index: (length(waveform)-5)
+        if waveform(i) > SD && waveform(i+1) > SD && waveform(i+2) > SD && waveform(i+3) > SD && waveform(i+4) > SD
+         SD_onset5_index = i;
+         SD_onset5_time = time(i);
+     break
+     else
+        SD_onset5_index = NaN;
+        SD_onset5_time = NaN;
+        end
+    end
+    if SD_offset4_index >= length(waveform)-5
+        SD_onset5_index = NaN;
+        SD_onset5_time = NaN;
+    end
+end
+
+%SD : Offset 5
+if isnan(SD_onset5_index)
+     SD_offset5_index = NaN;
+     SD_offset5_time = NaN;
+else
+    B = 0;
+    for i = SD_onset5_index:(length(waveform)-5)
+        c = [];
+        d = 1;
+        for f = i:(i+5) % cycles through here, window of 5 for the entire waveform
+            if waveform(f) < SD
+                c(d,:) = [f,1];
+            else
+                c(d,:) = [f,0];
+            end
+            B = sum(c(:,2)); % if there are 3 in the total 5, continues to identification of index and time
+            d = d+1;
+            if d == 6 % To break out of the loop
+                break
+            end
+        end
+        if B>=3
+            break
+        else 
+            B = 0;
+        end
+    end
+    
+         if B>=3      
+            for g = 1:5 % to iterate through the matrix
+                c1 = c(g,2);
+                try
+                if c1 == 1 % cycling through to find the first value 
+                    SD_offset5_index = c(g,1);
+                    SD_offset5_time = time(c(g,1));
+                    break
+                else
+                    SD_offset5_index = NaN; %if no value is found in the rotating set of 5, establishes as a NaN variable 
+                    SD_offset5_time = NaN;
+                end
+                catch
+                   SD_offset5_index = NaN; %establishes as a NaN variable 
+                   SD_offset5_time = NaN; 
+                end
+            end
+         else
+                SD_offset5_index = NaN; %establishes as a NaN variable 
+                SD_offset5_time = NaN;
+                       
+         end
+end
+
+
+
+
 % 
 % plot(time, waveform);
 % hold on
@@ -382,20 +793,74 @@ try SD_AUC2 = trapz(waveform(SD_onset2_index:SD_offset2_index));
 catch SD_AUC2 = NaN;
 end
 
-% Area 3: Onset 1 – Offset 2
-try MCD_AUC3 = trapz(waveform(MCD_onset1_index:MCD_offset2_index));
+%%%%%%% Area 1_2: Offset 1 – Onset 2
+try MCD_AUC1_2 = trapz(waveform(MCD_offset1_index:MCD_onset2_index));
+catch MCD_AUC1_2 = NaN;
+end
+
+try SD_AUC1_2 = trapz(waveform(SD_offset1_index:SD_onset2_index));
+catch SD_AUC1_2 = NaN;
+end
+
+% Area 3: Onset 3 – Offset 3
+try MCD_AUC3 = trapz(waveform(MCD_onset3_index:MCD_offset3_index));
 catch MCD_AUC3 = NaN;
 end
 
-try SD_AUC3 = trapz(waveform(SD_onset1_index:SD_offset2_index));
+try SD_AUC3 = trapz(waveform(SD_onset3_index:SD_offset3_index));
 catch SD_AUC3 = NaN;
 end
 
+%%%%%%% Area 2_3: Offset 2 – Onset 3
+try MCD_AUC2_3 = trapz(waveform(MCD_offset2_index:MCD_onset3_index));
+catch MCD_AUC2_3 = NaN;
+end
+
+try SD_AUC2_3 = trapz(waveform(SD_offset2_index:SD_onset3_index));
+catch SD_AUC2_3 = NaN;
+end
+
+% Area 4: Onset 4 – Offset 4
+try MCD_AUC4 = trapz(waveform(MCD_onset4_index:MCD_offset4_index));
+catch MCD_AUC4 = NaN;
+end
+
+try SD_AUC4 = trapz(waveform(SD_onset4_index:SD_offset4_index));
+catch SD_AUC4 = NaN;
+end
+
+%%%%%%% Area 3_4: Offset 3 – Onset 4
+try MCD_AUC3_4 = trapz(waveform(MCD_offset3_index:MCD_onset4_index));
+catch MCD_AUC3_4 = NaN;
+end
+
+try SD_AUC3_4 = trapz(waveform(SD_offset3_index:SD_onset4_index));
+catch SD_AUC3_4 = NaN;
+end
+
+% Area 5: Onset 5 – Offset 5
+try MCD_AUC5 = trapz(waveform(MCD_onset5_index:MCD_offset5_index));
+catch MCD_AUC5 = NaN;
+end
+
+try SD_AUC5 = trapz(waveform(SD_onset5_index:SD_offset5_index));
+catch SD_AUC5 = NaN;
+end
+
+%%%%%%% Area 4_5: Offset 4 – Onset 5
+try MCD_AUC4_5 = trapz(waveform(MCD_offset4_index:MCD_onset5_index));
+catch MCD_AUC4_5 = NaN;
+end
+
+try SD_AUC4_5 = trapz(waveform(SD_offset4_index:SD_onset5_index));
+catch SD_AUC4_5 = NaN;
+end
 
 
 Parameters(y,:) = [p MCD SD MCD_onset1_time MCD_offset1_time MCD_onset2_time...
-    MCD_offset2_time SD_onset1_time SD_offset1_time SD_onset2_time SD_offset2_time ...
-    MCD_AUC1 SD_AUC1 MCD_AUC2 SD_AUC2 MCD_AUC3 SD_AUC3];
+    MCD_offset2_time MCD_onset3_time MCD_offset3_time MCD_onset4_time MCD_offset4_time MCD_onset5_time MCD_offset5_time...
+    SD_onset1_time SD_offset1_time SD_onset2_time SD_offset2_time SD_onset3_time SD_offset3_time SD_onset4_time SD_offset4_time SD_onset5_time SD_offset5_time...
+    MCD_AUC1 SD_AUC1 MCD_AUC2 SD_AUC2 MCD_AUC1_2 SD_AUC1_2 MCD_AUC3 SD_AUC3 MCD_AUC2_3 SD_AUC2_3 MCD_AUC4 SD_AUC4 MCD_AUC3_4 SD_AUC3_4 MCD_AUC5 SD_AUC5 MCD_AUC4_5 SD_AUC4_5];
 
 clearvars -except Parameters y set 
 end
@@ -420,3 +885,29 @@ xlswrite(filename,Parameters(:,14),'Sheet1','N2')
 xlswrite(filename,Parameters(:,15),'Sheet1','O2')
 xlswrite(filename,Parameters(:,16),'Sheet1','P2')
 xlswrite(filename,Parameters(:,17),'Sheet1','Q2')
+xlswrite(filename,Parameters(:,18),'Sheet1','R2')
+
+xlswrite(filename,Parameters(:,19),'Sheet1','S2')
+xlswrite(filename,Parameters(:,20),'Sheet1','T2')
+xlswrite(filename,Parameters(:,21),'Sheet1','U2')
+xlswrite(filename,Parameters(:,22),'Sheet1','V2')
+xlswrite(filename,Parameters(:,23),'Sheet1','W2')
+xlswrite(filename,Parameters(:,24),'Sheet1','X2')
+xlswrite(filename,Parameters(:,25),'Sheet1','Y2')
+xlswrite(filename,Parameters(:,26),'Sheet1','Z2')
+xlswrite(filename,Parameters(:,27),'Sheet1','AA2')
+xlswrite(filename,Parameters(:,28),'Sheet1','AB2')
+xlswrite(filename,Parameters(:,29),'Sheet1','AC2')
+xlswrite(filename,Parameters(:,30),'Sheet1','AD2')
+xlswrite(filename,Parameters(:,31),'Sheet1','AE2')
+xlswrite(filename,Parameters(:,32),'Sheet1','AF2')
+xlswrite(filename,Parameters(:,33),'Sheet1','AG2')
+xlswrite(filename,Parameters(:,34),'Sheet1','AH2')
+xlswrite(filename,Parameters(:,35),'Sheet1','AI2')
+xlswrite(filename,Parameters(:,36),'Sheet1','AJ2')
+
+xlswrite(filename,Parameters(:,37),'Sheet1','AK2')
+xlswrite(filename,Parameters(:,38),'Sheet1','AL2')
+xlswrite(filename,Parameters(:,39),'Sheet1','AM2')
+xlswrite(filename,Parameters(:,40),'Sheet1','AN2')
+xlswrite(filename,Parameters(:,41),'Sheet1','AO2')
